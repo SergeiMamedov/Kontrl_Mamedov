1f5d516e8ca6d9fa9c7b6c806c989d0d276835f4
Sergei <sergeimamedov9976@gmail.com>
2023-03-06 21:58:20 +0600

    функции

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,3 +2,20 @@
 // длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+string[] FillArray(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Prompt($"Write {i} element: ");
+    }
+
+    return arr;
+}
